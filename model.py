6704496212bb4f734bcc9a2afd90ece70b696377
Darkhan <turtayev.darkhan@gmail.com>
2025-04-20 03:02:02 +0500

    model update

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import torch 
 
+torch.cuda.is_available()
 df_train = pd.read_csv('db_train.csv')
 df_test = pd.read_csv('db_test.csv')
 
@@ -9,3 +11,4 @@ head_db = df_train.head()
 df_full = pd.concat([df_train, df_test], ignore_index=True)
 
 print(df_full.head())
+
